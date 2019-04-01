8f8af76ce38fe3b27f68502e45b30420a6e5c305
Terence Chiu <tchiu2@gmail.com>
2019-04-01 12:58:56 -0400

    add db setup scripts

--- server/config/config.js
+++ server/config/config.js
@@ -4,21 +4,21 @@ module.exports = {
   "development": {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE,
+    "database": "atlas-development",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE,
+    "database": "atlas-test",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE,
+    "database": "atlas-production",
     "host": "127.0.0.1",
     "dialect": "postgres"
   }
