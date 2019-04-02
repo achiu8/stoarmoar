52d4efd7c0e1d97b703ceb3cf3c4ed2a30817f51
Terence Chiu <tchiu2@gmail.com>
2019-04-02 13:18:00 -0400

    refactor models index

--- server/models/index.js
+++ server/models/index.js
@@ -7,12 +7,9 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 
-const sequelizeArgs = 
-  config.use_env_variables 
-  ? [process.env[config.use_env_variable]]
-  : [config.database, config.username, config.password];
-
-const sequelize = new Sequelize(...sequelizeArgs, config);
+const sequelize = config.use_env_variable
+  ? new Sequelize(process.env[config.use_env_variable], config)
+  : new Sequelize(config.database, config.username, config.password, config);
 
 const db = fs
   .readdirSync(__dirname)
