4801754734cd93affaf387e0a6d46523214ff23a
achiu8 <achiu8@gmail.com>
2019-06-11 20:31:41 -0400

    remove dropbox login

--- src/SocialAuth.js
+++ src/SocialAuth.js
@@ -2,33 +2,23 @@ import React, { Component } from 'react';
 
 class SocialAuth extends Component {
   state = {
-    google: '',
-    dropbox: ''
+    google: ''
   };
 
   componentDidMount() {
     fetch('/google/auth-url')
       .then(res => res.json())
       .then(({ url }) => this.setState({ google: url }));
-
-    fetch('/dropbox/auth-url')
-      .then(res => res.json())
-      .then(({ url }) => this.setState({ dropbox: url }));
   }
 
   render() {
-    const { google, dropbox } = this.state;
+    const { google } = this.state;
 
     return (
       <div>
         {google && (
           <div>
             <a href={google}>Login with Google</a>
-          </div>
-        )}
-        {dropbox && (
-          <div>
-            <a href={dropbox}>Login with Dropbox</a>
           </div>
         )}
       </div>
