74aa8d1f328e2f5943f76cf3fa546ddc828dc8df
achiu8 <achiu8@gmail.com>
2019-06-15 14:03:14 -0400

    add avatar and menu to header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Avatar, Col, Layout, Row } from 'antd';
+import { Avatar, Col, Dropdown, Layout, Menu, Row } from 'antd';
+
+const menu = (
+  <Menu>
+    <Menu.Item>Settings</Menu.Item>
+    <Menu.Item>Log out</Menu.Item>
+  </Menu>
+);
 
 export default () => (
   <Layout.Header>
     <Row type="flex" justify="end">
       <Col>
+        <Dropdown placement="bottomRight" overlay={menu}>
           <Avatar icon="user" />
+        </Dropdown>
       </Col>
     </Row>
   </Layout.Header>
