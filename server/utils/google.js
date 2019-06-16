1ecd596a0458c2dc1d6412346c45653f3b474d09
achiu8 <achiu8@gmail.com>
2019-06-16 00:02:10 -0400

    remove more unused code

--- server/utils/google.js
+++ server/utils/google.js
@@ -1,6 +1,3 @@
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
-const path = require('path');
 const { google } = require('googleapis');
 
 const SCOPES = [
@@ -8,9 +5,6 @@ const SCOPES = [
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/userinfo.email'
 ];
-
-const TOKEN_PATH = path.resolve(__dirname, '../google_token.json');
-const FILES_PATH = path.resolve(__dirname, '../google_files.json');
 
 const authClient = () =>
   new google.auth.OAuth2(
@@ -30,16 +24,6 @@ const getToken = code =>
     .then(({ tokens }) => tokens)
     .catch(err => console.log('Error fetching token:', err));
 
-const saveToken = code =>
-  authClient().getToken(code)
-    .then(({ tokens }) => fs.writeFileAsync(TOKEN_PATH, JSON.stringify(tokens)))
-    .catch(err => console.log('Error saving token:', err));
-
-const loadToken = () =>
-  fs.readFileAsync(TOKEN_PATH)
-    .then(data => JSON.parse(data))
-    .catch(err => console.log('Error loading token:', err));
-
 const isFolder = ({ id, mimeType }) =>
   id === 'root' || mimeType === 'application/vnd.google-apps.folder';
 
@@ -47,6 +31,5 @@ module.exports = {
   authClient,
   authUrl,
   getToken,
-  saveToken,
   isFolder
 };
