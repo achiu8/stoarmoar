55a8eb10b52bdecf805d95ac79edcddad68d6c90
achiu8 <achiu8@gmail.com>
2019-06-17 21:50:04 -0400

    redefine account types; delete sample data

--- src/AddAccount.js
+++ src/AddAccount.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Col, Icon, Modal, Row } from 'antd';
 
-import { chunksOf } from './utils';
-import { accountTypes } from './sample_data';
+import { chunksOf, accountName } from './utils';
+import accountTypes from './utils/account_types';
 
 export default props => (
   <Modal title="Add Account" footer={null} {...props}>
     {chunksOf(4, accountTypes).map((row, i) => (
       <Row key={i} className="grid-row" gutter={48}>
-        {row.map(({ id, name }) => (
+        {row.map(id => (
           <Col key={id} span={6}>
             <div className="grid-item">
               <Icon className="grid-item-icon" type={id} />
-              <span>{name}</span>
+              <span>{accountName(id)}</span>
             </div>
           </Col>
         ))}
