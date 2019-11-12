d354ec434384343e354cf06cfc3a2dfb00e957f5
achiu8 <achiu8@gmail.com>
2019-11-11 19:31:16 -0500

    delete controllers index

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const users = require('../controllers').users;
+const users = require('../controllers/users');
 
 router.get('/current', users.current);
 router.get('/users', users.list);
