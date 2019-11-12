8822c180555ed2413b7740580460cefd2b157e49
achiu8 <achiu8@gmail.com>
2019-11-11 20:07:41 -0500

    use json body parser

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const auth = require('./middlewares/auth');
 const api = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
 
 app.use('/api', auth, api);
 
