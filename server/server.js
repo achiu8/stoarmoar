608593318ba5430a21f9ff757c4721fef72f6d71
achiu8 <achiu8@gmail.com>
2019-11-11 19:26:08 -0500

    temporarily remove dropbox routes

--- server/server.js
+++ server/server.js
@@ -2,13 +2,11 @@ require('dotenv').config()
 const express = require('express');
 
 const auth = require('./middlewares/auth');
-const dropbox = require('./routes/dropbox');
 const api = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use('/dropbox', dropbox);
 app.use('/api', auth, api);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
