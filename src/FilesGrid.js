29b367d1aa9d5833f864cb741923a18d1e49e313
achiu8 <achiu8@gmail.com>
2019-11-15 23:18:54 -0500

    add drag and drop to grid view

--- src/FilesGrid.js
+++ src/FilesGrid.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { Col, Icon, Row } from 'antd';
+import classNames from 'classnames';
 
+import DragAndDrop from './DragAndDrop';
 import { chunksOf, filename, filesForAccount } from './utils';
 
+const position = (cs, i, j) => cs * i + j;
+
 export default ({ account, columns, files, onMove, onNavigate }) => (
-  chunksOf(columns, filesForAccount(account, files)).map((row, i) => (
-    <Row key={i} className="grid-row" gutter={48}>
-      {row.map(({ name, type }, j) => (
-        <Col key={j} span={24 / columns}>
-          <div
-            className="grid-item"
-            onClick={() => onNavigate(type, name, columns * i + j)}
-          >
-            <Icon className="grid-item-icon" type={type} />
-            <span>{filename(name)}</span>
-          </div>
-        </Col>
-      ))}
-    </Row>
-  ))
+  <DragAndDrop.Provider>
+    {chunksOf(columns, filesForAccount(account, files)).map((row, i) => (
+      <Row key={i} className="grid-row" gutter={48}>
+        {row.map(({ name, type }, j) => (
+          <Col key={j} span={24 / columns}>
+            <DragAndDrop.Draggable i={position(columns, i, j)} render={draggableProps => (
+              <DragAndDrop.Droppable
+                droppable={type === 'folder'}
+                i={position(columns, i, j)}
+                onDrop={onMove}
+                render={({ dropping, ...droppableProps }) => (
+                  <div
+                    {...draggableProps}
+                    {...droppableProps}
+                    className={classNames('grid-item', {
+                      'Droppable-hovered': dropping
+                    })}
+                    onClick={() => onNavigate(type, name, position(columns, i, j))}
+                  >
+                    <Icon className="grid-item-icon" type={type} />
+                    <span>{filename(name)}</span>
+                  </div>
+                )}
+              />
+           )} />
+          </Col>
+        ))}
+      </Row>
+    ))}
+  </DragAndDrop.Provider>
 );
