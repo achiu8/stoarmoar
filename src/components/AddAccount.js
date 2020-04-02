301ff9b6450e6a915dd371874eeb1ea8eb0de5e3
achiu8 <achiu8@gmail.com>
2020-04-02 19:16:57 -0400

    convert add account to list

--- src/components/AddAccount.js
+++ src/components/AddAccount.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Col, Icon, Modal, Row } from 'antd';
-import { Link } from "react-router-dom";
+import { Modal } from 'antd';
 
-import { chunksOf, accountName } from '../utils';
+import Providers from './Providers';
 import accountTypes from '../utils/account_types';
 
 import '../styles/AddAccount.css';
 
 export default props => (
-  <Modal title="Add Account" footer={null} {...props}>
-    {chunksOf(4, accountTypes).map((row, i) => (
-      <Row key={i} className="grid-row" gutter={[48, 48]}>
-        {row.map(id => (
-          <Col key={id} span={6}>
-            <div className="grid-item" href="www.#.com">
-              <Link className="grid-item" to="www.#.com">
-                <Icon className="grid-item-icon" type={id} />
-                <span>{accountName(id)}</span>
-              </Link>
-            </div>
-          </Col>
-        ))}
-      </Row>
-    ))}
+  <Modal title="Add Account" footer={null} width={300} {...props}>
+    <Providers providers={accountTypes} onClick={() => {}} />
   </Modal>
 );
